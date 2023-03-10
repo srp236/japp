a2883944a213755d63cbfb40bda4fb23be2a9ad1
Powell <sheyeanne.powell@cgi.com>
2023-03-09 21:02:13 -0500

    added notifs and confs, general clean up

--- src/pages/songs/[song].js
+++ src/pages/songs/[song].js
@@ -1,68 +1,48 @@
-// import Layout from '../../../components/layout';
-import Head from 'next/head';
-import Link from 'next/link';
-import Image from 'next/image'
-import { Layout, Row, Col, Spin, Card, Menu, Dropdown, Space} from 'antd';
-import styles from '@/src/styles/Home.module.css'
-import logo from '../../../public/images/logo_red.png'
-import { useRouter } from "next/router"
-import React, { useState, useEffect } from 'react'
-import { EditOutlined, MoreOutlined } from '@ant-design/icons';
-import addData from '@/src/firebase/firestore/addData'
 import { getData, getAllDocs} from '@/src/firebase/firestore/getData'
+import { Layout, Row, Col, Spin, Card, Dropdown } from 'antd';
 import delField from '@/src/firebase/firestore/delField'
+import logo from '../../../public/images/logo_red.png'
+import addData from '@/src/firebase/firestore/addData'
+import { MoreOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react'
+import styles from '@/src/styles/Home.module.css'
+import { useRouter } from "next/router"
+import Image from 'next/image'
+import Head from 'next/head';
 
-const { Header, Footer, Content } = Layout;
+const { Header, Footer } = Layout;
+const items = [];
 
-const flashCardDoc = async (k,m) => {
-  const request = await getAllDocs('flashcards',)
+const flashCardDoc = async () => {
+  const request = await getAllDocs('flashcards')
   request.map((it=>{
     let i=items.length
     items.push({label:it, key: i,})
   }))
 }
 
-const items = [
-  // {
-  //   type: 'divider',
-  // },
-  // {
-  //   label:'new +',
-  //   key:0
-  // }
-];
-
 flashCardDoc()
 
 export function Dropdwn({kanji, meaning}) {
   return (
     <Dropdown
-    overlayStyle={{width:'fit-content'}}
-    menu={{items:items, selectable:true, onClick:(e)=>{
-      if(items[e.key].label == 'new +'){
-        console.log('creating a new set')
-        console.log(e)
-      } else {
-        getData('flashcards',items[e.key].label).then((res)=>{
-          if(res.result.data()[kanji] == undefined){
-            storeCard(items[e.key].label,kanji,meaning)
-            console.log('card added to set')
-        }
-        else{
-          delCard(items[e.key].label,kanji)
-          console.log('card removed from set')
-        }
-      })
-      }
-    }}}
-    trigger={['click']}
-  >
-    <a onClick={(e) => e.preventDefault()}>
-      <Space>
-        <MoreOutlined style={{color:'red', fontSize:'20px', position:'absolute', right:'10px', top:'20px'}}/>
-      </Space>
-    </a>
-  </Dropdown>
+      overlayStyle={{width:'fit-content'}}
+      menu={{items:items, selectable:true, onClick:(e)=>{
+        if(items[e.key].label == 'new +'){
+          console.log('creating a new set')
+          console.log(e)
+        } else {
+          getData('flashcards',items[e.key].label).then((res)=>{
+            if(res.result.data()[kanji] == undefined){
+              storeCard(items[e.key].label,kanji,meaning)
+              console.log('card added to set')
+            }
+            else{
+              confirm(`Are you sure you want to remove ${kanji} from "${items[e.key].label}"`)?delCard(items[e.key].label,kanji):console.log('user canceled')
+            }
+          })}}}} trigger={['click']}>
+      <MoreOutlined onClick={(e) => e.preventDefault()} style={{color:'red', fontSize:'20px', position:'absolute', right:'10px', top:'20px'}}/>
+    </Dropdown>
   )
 }
 
@@ -71,8 +51,6 @@ const storeCard = async (set, kanjichar, meaning) => {
   const { result, error } = await addData('flashcards', set, data)
   if (error) {
       return console.log(error)
-  } else {
-    console.log('card added to FS')
   }
 }
 
@@ -81,16 +59,14 @@ const delCard = async (set, kanjichar) => {
   const { result, error } = await delField('flashcards', set, data)
   if (error) {
       return console.log(error)
-  } else {
-    console.log('card removed')
   }
 }
 
 export default function Song() {
-  const [loading, setLoading] = useState(true);
-  const [info, setInfo] = useState([]);
   const router = useRouter()
   const {query: { title, artist, fstat }} = router 
+  const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState([]);
 
   async function scrapeData(ref, songN, artistN) {
     const data = {
@@ -102,12 +78,9 @@ export default function Song() {
     const endpoint = '/api/get_lyrics'
     const options = {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: {'Content-Type': 'application/json'},
       body: JSONdata,
     }
-  
     const request = await fetch(endpoint, options)
     const response = await request.json()
   
@@ -118,53 +91,30 @@ export default function Song() {
     }
   }
 
-  async function getKanjiInfo(list) {
-    let i = 0;
-    let myList = []
-    let k
-    let s
+ async function getKanjiInfo(list) {
+    let i = 0, k, s, myList = []
     while (i< (list).length) {
       k=list[i]
       await fetch(`https://kanjiapi.dev/v1/kanji/${k}`).then(r => r.json()).then(r=> s=r);
-      myList.push({kanji:s.kanji, jlpt:s.jlpt, kun:s.kun_readings, onr:s.on_readings, meaning: s.meanings, value: 0})
+      myList.push({kanji:s.kanji, jlpt:s.jlpt, kun:s.kun_readings, onr:s.on_readings, meaning: s.meanings, key: i, bl:false})
       i++
     }
     return myList
   }
 
   const KanjiList = () => {
-    const mItems = [
-      {
-        label: <a href="https://www.antgroup.com">1st menu item</a>,
-        key: '0',
-      },
-      {
-        label: <a href="https://www.aliyun.com">2nd menu item</a>,
-        key: '1',
-      },
-      {
-        type: 'divider',
-      },
-      {
-        label: '3rd menu item',
-        key: '3',
-      },
-    ];
     return (
       info.map(item=>(
-        <Card className={styles.card}>
+        <Card className={styles.card} key={item.key}>
           <div className={styles.kanjiCard}>
             <h2 onClick={()=>{
               let color
-              item.value++
-              item.value == 1? color = 'yellow':color='white'
+              item.bl = !item.bl
+              item.bl? color = 'yellow': color='white'
               const div = document.querySelector('pre')
               div.innerHTML = div.innerHTML.replaceAll(item.kanji,()=>{
                 return `<span style="background-color: ${color}">${item.kanji}</span>`
               })
-              if(item.value >1){
-                item.value = 0
-              }
             }} 
             className={styles.cardL}>{item.kanji}</h2>
             <div className={styles.cardR}>
@@ -222,21 +172,17 @@ export default function Song() {
         </div>
       </Header>
       <div className={styles.bar}></div>
-      <h1>{title}</h1>
-      <h4>{artist}</h4>
       <div className={styles.lbody}>
         <Card style={{width:'33%',}}>
-          <pre id='lyrics' style={{fontSize:'15px',}}></pre>
+          <h1>{title}</h1>
+          <h4>{artist}</h4>
+          <pre id='lyrics' style={{fontSize:'15px', marginTop:'20px'}}></pre>
         </Card> 
         <Card id='may' style={{width:'33%',overflow:'scroll',}}>
           <KanjiList/>
         </Card>
         <iframe title='jisho' style={{width:'33%'}} src='https://jisho.org/'></iframe>
       </div>
-      <form>
-        <input style={{height:'300px', width:'100%'}} type='text' id='songName' name='songName'/>
-        <button type='submit'>Save</button>  
-      </form>
       <Footer className={styles.footerStyle}>
         <Row>
           {/* <Col span={8}>temp</Col> */}
