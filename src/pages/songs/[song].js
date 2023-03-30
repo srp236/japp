8fcb103493d6430a3fc06d6a56cb99d5fe9e204f
Powell <sheyeanne.powell@cgi.com>
2023-03-29 22:47:00 -0400

    started adding song annotation feature

--- src/pages/songs/[song].js
+++ src/pages/songs/[song].js
@@ -1,5 +1,5 @@
 import { getData, getDocuQuery } from '@/src/firebase/firestore/getData'
-import { Layout, Spin, Card } from 'antd';
+import { Layout, Spin, Card, Drawer, Modal, Popover } from 'antd';
 import React, { useState, useEffect } from 'react'
 import styles from '@/src/styles/Home.module.css'
 import { useRouter } from "next/router"
@@ -8,14 +8,17 @@ import Head from 'next/head';
 import { KanjiList, CommonFoot, flashCardDoc } from '@/src/utils/methods';
 import { useAuth } from '@/src/utils/AuthUserContext';
 const { Header } = Layout;
+import { MenuOutlined, DeleteOutlined, FileAddOutlined } from '@ant-design/icons';
 
 export default function Song() {
   const router = useRouter()
   const {query: { title, artist, fstat }} = router 
   const [sloading, setsLoading] = useState(true);
+  const [selec, setSelec] = useState();
   const [info, setInfo] = useState([]);
+  const [open, setOpen] = useState(false);
   const { authUser, loading } = useAuth();
-  let name ='', uid =''
+  let name ='', uid ='', hi = undefined
   authUser?[name=authUser.name, uid = authUser.uid]:null
 
   async function scrapeData(ref, songN, artistN) {
@@ -45,7 +48,7 @@ export default function Song() {
     if(fstat == 0) {
       const request = await getData('lyrics', artist)
       const response = request.result.data()[title]
-      document.getElementById('lyrics').innerHTML = response.lyrics
+      document.getElementById('lyrics').innerText = response.lyrics
       const request2 = await getDocuQuery('kanji', 'songRef', 'array-contains', `${title} by ${artist}`)
       setInfo(request2)
       setsLoading(false)
@@ -54,14 +57,52 @@ export default function Song() {
       if(res == 0){
         const request = await getData('lyrics', artist)
         const response = request.result.data()[title]
-        document.getElementById('lyrics').innerHTML = response.lyrics
+        document.getElementById('lyrics').innerText = response.lyrics
         const request2 = await getDocuQuery('kanji', 'songRef', 'array-contains', `${title} by ${artist}`)
         setInfo(request2)
         setsLoading(false)
       }
     }
   }
+  
+  function getText() {
+    setSelec(window.getSelection())
+    let sel = window.getSelection()
+    // let num = Math.floor(Math.random()*100)
+    // let spn = document.createElement("a")
+    // spn.setAttribute("href",`${num}/${title}/${sel}`)
+    // let ob = document.createElement("span")
+    // ob.appendChild(document.createTextNode(sel))
+    // spn.append(ob)
+    // let range = sel.getRangeAt(0)
+    // range.deleteContents()
+    // range.insertNode(spn)
+    // let qw = sel.deleteFromDocument()
+    // console.log(qw)
 
+    // if(!sel.toString().trim().length){
+    //   return
+    // }
+    // let tool = document.getElementById('tltp')
+    let rect = sel.getRangeAt(0).getBoundingClientRect()
+    document.getElementById('tltp').style.visibility = "visible";
+    document.getElementById('tltp').style.top = `${rect.bottom + 10}px`;
+    document.getElementById('tltp').style.left = `${rect.x}px`;
+    // const div = document.querySelector('pre')
+    // div.classList.toggle("tlt p")
+    // div.innerHTML = div.innerHTML.replace(sel,()=>{
+    //   return `<a style={{display:'inline'}} >${sel}</a>`
+    // });    
+    // yu.insertAdjacentHTML("afterend","<div style={{backgroundColor:'red'}}>hiya</div>");
+    // let rect = sel.getRangeAt(0).getBoundingClientRect()
+    // document.getElementById('tltp').style.visibility = "visible";
+    // document.getElementById('tltp').style.right = `${rect.y}px`;
+    // document.getElementById('tltp').style.bottom = `${rect.top}px`;
+    // const tooltip = document.getElementById('tltp')
+    // tooltip.style.display = "block"
+    // <Tooltip title='thththth'style={{position:'absolute', top:rect.bottom, left:rect.y}} ></Tooltip>
+  }
+ 
   useEffect(()=>{
     setsLoading(true)
     if(!loading && !authUser){
@@ -87,19 +128,56 @@ export default function Song() {
         <div onClick={()=>{router.push('/home')}}>
           <Image alt='logo' height={50} width={120} src='/images/logo_red.png' />
         </div>
+          <MenuOutlined onClick={()=>{setOpen(true)}} />
       </Header>
       <div className={styles.bar}></div>
       <div className={styles.lbody}>
-        <Card style={{width:'30%'}}>
-          <h1>{title}</h1>
+        <Card style={{width:'40%'}}>
+          <h1 id='til'>{title}</h1>
           <h4>{artist}</h4>
-          <pre id='lyrics' style={{fontSize:'15px', marginTop:'20px'}}></pre>
+          <div className={styles.ty} id='lyrics' style={{fontSize:'15px', marginTop:'20px'}} 
+          onPointerDown={()=>{document.getElementById('tltp').style.visibility = "hidden";}} 
+          onPointerUp={()=>{getText();}}></div>
         </Card> 
-        <Card id='may' style={{width:'50%',overflow:'scroll'}}>
+        <Card id='may' style={{width:'80%',overflow:'scroll'}}>
           <KanjiList info={info} uid={uid}/>
         </Card>
-        <iframe title='dictionary' style={{width:'35%'}} src='https://jisho.org/' ></iframe>
+        <button id='tltp' className={styles.tltp}>
+          <DeleteOutlined onClick={()=>{}}/>
+          <FileAddOutlined onClick={()=>{}}/>
+          <button style={{backgroundColor:'pink'}} className={styles.highlightOptions} onClick={()=>{
+            let num = Math.floor(Math.random()*100)
+            let spn = document.createElement("a")
+            spn.setAttribute("href",`#${num}`)
+            // spn.setAttribute("href",`${title}/${num}`)
+            let ob = document.createElement("span")
+            ob.classList.add(`${styles.gotta}`)
+            ob.appendChild(document.createTextNode(selec))
+            spn.append(ob)
+            let range = selec.getRangeAt(0)
+            range.deleteContents()
+            range.insertNode(spn)
+            
+            let note = document.getElementById("notes")
+            let puh = document.createElement("div")
+            puh.setAttribute("id", `${num}`)
+            puh.append(selec)
+            note.insertAdjacentElement("beforeend", puh)
+            document.getElementById('tltp').style.visibility = "hidden";
+            //somewhere here i need to add to db under some notes section for the song specific to the user
+          }}></button>
+          <button style={{backgroundColor:'grey'}} className={styles.highlightOptions} onClick={()=>{
+            const div = document.querySelector('pre')
+            div.innerHTML = div.innerHTML.replace(selec,()=>{
+              return `<span style="background-color: grey">${selec}</span>`
+            }); 
+          }}></button>
+        </button>
       </div>
+      <Drawer title="jisho_drawer" placement='right'>
+        <iframe title='dictionary' style={{width:'35%'}} src='https://jisho.org/'></iframe>
+      </Drawer>
+      <Card id='notes'></Card>
       <CommonFoot/>
       </Layout>
     </Spin>
